dc3c8e3f050ae485152dd34514c5d4f44c70876e
SeungYeop Yang <seungyeop.yang@ibm.com>
2022-01-31 22:02:29 -0600

    pointer and errors and the first go.sum

---
+++ 07-pointers-errors/v1/wallet.go
@@ -0,0 +1,13 @@
+package main
+
+type Wallet struct {
+	balance int
+}
+
+func (w *Wallet) Deposit(deposit int) {
+	w.balance += deposit
+}
+
+func (w *Wallet) Balance() int {
+	return w.balance
+}
---
+++ 07-pointers-errors/v2/wallet.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+type Stringer interface {
+	String() string
+}
+
+type Bitcoin int
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+type Wallet struct {
+	balance Bitcoin
+}
+
+func (w *Wallet) Deposit(deposit Bitcoin) {
+	w.balance += deposit
+}
+
+var ErrInsufficientFunds = errors.New("cannot withdraw, insufficient funds")
+
+func (w *Wallet) Withdraw(withdraw Bitcoin) error {
+
+	if withdraw > w.balance {
+		return ErrInsufficientFunds
+	}
+	w.balance -= withdraw
+	return nil
+}
+
+func (w *Wallet) Balance() Bitcoin {
+	return w.balance
+}
